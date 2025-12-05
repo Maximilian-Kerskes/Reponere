55c50b2ccc262c6edc56c3b94fa08cb470a0a8be
Maximilian Kerskes <kerskesmaximilian@gmail.com>
2025-12-05 17:08:25 +0100

    bang

---
+++ src/build/parse.rs
@@ -0,0 +1,21 @@
+use crate::build::package::*;
+use std::{fs::File, io};
+
+pub struct PackageParser {
+    filename: String,
+}
+
+impl PackageParser {
+    pub fn new<S: Into<String>>(filename: S) -> PackageParser {
+        PackageParser {
+            filename: filename.into(),
+        }
+    }
+
+    pub fn parse(&self) -> Result<Package, Box<dyn std::error::Error>> {
+        let file = File::open(&self.filename)?;
+        let reader = io::BufReader::new(file);
+        let package = serde_yml::from_reader(reader)?;
+        Ok(package)
+    }
+}
